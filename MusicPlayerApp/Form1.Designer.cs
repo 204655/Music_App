c3e95e10004f11ea4546531ad0160c377b536569
Adam <l3vanha70h79c@gmail.com>
2024-09-14 14:31:43 +0700

    Update Form1.Designer.cs

--- MusicPlayerApp/Form1.Designer.cs
+++ MusicPlayerApp/Form1.Designer.cs
@@ -30,12 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerApp));
             this.TopPanel = new System.Windows.Forms.Panel();
-            this.lblLogo = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblLogo = new System.Windows.Forms.Label();
             this.listBoxSongs = new System.Windows.Forms.ListBox();
             this.btnSelectSongs = new System.Windows.Forms.Button();
             this.axWindowsMediaPlayerMusic = new AxWMPLib.AxWindowsMediaPlayer();
-            this.lblFooter = new System.Windows.Forms.Label();
             this.TopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerMusic)).BeginInit();
@@ -49,18 +48,8 @@
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.Location = new System.Drawing.Point(0, 0);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(680, 35);
+            this.TopPanel.Size = new System.Drawing.Size(859, 35);
             this.TopPanel.TabIndex = 0;
-            // 
-            // lblLogo
-            // 
-            this.lblLogo.AutoSize = true;
-            this.lblLogo.Font = new System.Drawing.Font("Nexa Rust Script L0", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogo.Location = new System.Drawing.Point(21, 10);
-            this.lblLogo.Name = "lblLogo";
-            this.lblLogo.Size = new System.Drawing.Size(103, 17);
-            this.lblLogo.TabIndex = 0;
-            this.lblLogo.Text = "Music Player App";
             // 
             // pictureBox1
             // 
@@ -73,12 +62,22 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.Location = new System.Drawing.Point(21, 10);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(112, 16);
+            this.lblLogo.TabIndex = 0;
+            this.lblLogo.Text = "Music Player App";
+            // 
             // listBoxSongs
             // 
             this.listBoxSongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBoxSongs.FormattingEnabled = true;
             this.listBoxSongs.ItemHeight = 16;
-            this.listBoxSongs.Location = new System.Drawing.Point(485, 52);
+            this.listBoxSongs.Location = new System.Drawing.Point(653, 52);
             this.listBoxSongs.Name = "listBoxSongs";
             this.listBoxSongs.Size = new System.Drawing.Size(183, 244);
             this.listBoxSongs.TabIndex = 1;
@@ -89,7 +88,7 @@
             this.btnSelectSongs.BackColor = System.Drawing.Color.OrangeRed;
             this.btnSelectSongs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSelectSongs.ForeColor = System.Drawing.Color.White;
-            this.btnSelectSongs.Location = new System.Drawing.Point(485, 303);
+            this.btnSelectSongs.Location = new System.Drawing.Point(653, 303);
             this.btnSelectSongs.Name = "btnSelectSongs";
             this.btnSelectSongs.Size = new System.Drawing.Size(183, 49);
             this.btnSelectSongs.TabIndex = 2;
@@ -100,28 +99,17 @@
             // axWindowsMediaPlayerMusic
             // 
             this.axWindowsMediaPlayerMusic.Enabled = true;
-            this.axWindowsMediaPlayerMusic.Location = new System.Drawing.Point(10, 52);
+            this.axWindowsMediaPlayerMusic.Location = new System.Drawing.Point(12, 52);
             this.axWindowsMediaPlayerMusic.Name = "axWindowsMediaPlayerMusic";
             this.axWindowsMediaPlayerMusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayerMusic.OcxState")));
-            this.axWindowsMediaPlayerMusic.Size = new System.Drawing.Size(467, 300);
+            this.axWindowsMediaPlayerMusic.Size = new System.Drawing.Size(635, 300);
             this.axWindowsMediaPlayerMusic.TabIndex = 3;
-            // 
-            // lblFooter
-            // 
-            this.lblFooter.AutoSize = true;
-            this.lblFooter.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lblFooter.Location = new System.Drawing.Point(285, 364);
-            this.lblFooter.Name = "lblFooter";
-            this.lblFooter.Size = new System.Drawing.Size(136, 13);
-            this.lblFooter.TabIndex = 4;
-            this.lblFooter.Text = "Developed By: Vijay Thapa";
             // 
             // MusicPlayerApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 387);
-            this.Controls.Add(this.lblFooter);
+            this.ClientSize = new System.Drawing.Size(859, 580);
             this.Controls.Add(this.axWindowsMediaPlayerMusic);
             this.Controls.Add(this.btnSelectSongs);
             this.Controls.Add(this.listBoxSongs);
@@ -135,7 +123,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerMusic)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -147,7 +134,6 @@
         private System.Windows.Forms.ListBox listBoxSongs;
         private System.Windows.Forms.Button btnSelectSongs;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayerMusic;
-        private System.Windows.Forms.Label lblFooter;
     }
 }
 
